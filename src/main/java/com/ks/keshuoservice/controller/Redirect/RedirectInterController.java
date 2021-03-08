b75fa3e5de384080e24f16321b6b75b666eb4104
renzhiqiang1 <627790136@qq.com>
2021-03-08 23:19:48 +0800

    添加1

--- src/main/java/com/ks/keshuoservice/controller/Redirect/RedirectInterController.java
+++ src/main/java/com/ks/keshuoservice/controller/Redirect/RedirectInterController.java
@@ -2,6 +2,8 @@ package com.ks.keshuoservice.controller.Redirect;
 
 import com.ks.keshuoservice.service.Redirect.RedirectInterService;
 import com.ks.keshuoservice.utils.common.PageData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.io.IOException;
 
 @RestController
 public class RedirectInterController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private RedirectInterService redirectInterService;
@@ -21,6 +24,7 @@ public class RedirectInterController {
     public void redirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PageData pd = new PageData(request);
         String url = redirectInterService.redirect(pd);
+        logger.info("inter/redirect下游传参："+pd.toString()+" 重定向上游url地址:"+url);
         response.sendRedirect(url);
     }
 
